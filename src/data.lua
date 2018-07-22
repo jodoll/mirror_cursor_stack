4cea816e2d94eebc1975f8523c2614d7c80c0e43
Johannes Doll <doll.johannes.94@gmail.com>
2018-07-22 23:45:04 +0200

    Add vertical mirroring and change key bindings

--- src/data.lua
+++ src/data.lua
@@ -1,6 +1,9 @@
 local defines = {
     input = {
-        mirror = "input-event-mirror-item"
+        mirror_horizontal_a = "input-event-mirror-item-horizontal-a",
+        mirror_horizontal_d = "input-event-mirror-item-horizontal-d",
+        mirror_vertical_w = "input-event-mirror-item-vertical-w",
+        mirror_vertical_s = "input-event-mirror-item-vertical-s"
     }
 }
 
@@ -9,8 +12,29 @@ if (data ~= nil) then
     data:extend({
         {
             type = "custom-input",
-            name = defines.input.mirror,
-            key_sequence = "SHIFT + R",
+            name = defines.input.mirror_horizontal_a,
+            key_sequence = "SHIFT + A",
+            consuming = "all"
+
+        },
+        {
+            type = "custom-input",
+            name = defines.input.mirror_horizontal_d,
+            key_sequence = "SHIFT + D",
+            consuming = "all"
+
+        },
+        {
+            type = "custom-input",
+            name = defines.input.mirror_vertical_w,
+            key_sequence = "SHIFT + W",
+            consuming = "all"
+
+        },
+        {
+            type = "custom-input",
+            name = defines.input.mirror_vertical_s,
+            key_sequence = "SHIFT + S",
             consuming = "all"
 
         }
