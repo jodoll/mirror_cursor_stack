ca505cd3738bc462681547e84537120d11a870ed
Johannes Doll <doll.johannes.94@gmail.com>
2018-07-22 20:20:51 +0200

    Change point to dynamically resolve access

--- src/mirror/blueprint_mirror_function.lua
+++ src/mirror/blueprint_mirror_function.lua
@@ -8,22 +8,22 @@ function BluePrintMirrorFunction:new(entities)
     return self.object
 end
 
-function BluePrintMirrorFunction:mirror()
-    return self:calculate_size()
-end
-
-function BluePrintMirrorFunction:calculate_size()
+function BluePrintMirrorFunction:mirror_horizontal()
     local result = ""
     for key, entity in pairs(self.entities) do
+        --self:mirror_position_horizontal(Point:new(entity))
         result = result .. self:entity_to_string(entity)
     end
     return result
+end
+
+function BluePrintMirrorFunction:mirror_position_horizontal(point)
 end
 
 function BluePrintMirrorFunction:entity_to_string(entity)
     local position = Point:new(entity.position)
     local name = entity.name
 
-    local result = result .. "Entity: " .. name
-    return result .. "Position: " .. position:to_string()
+    local result = "Entity: " .. name
+    return result .. " Position: " .. position:to_string()
 end
