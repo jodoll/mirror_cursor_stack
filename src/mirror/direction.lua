7909611215bea64570ee16e3fab6a983a4c03e4d
Johannes Doll <doll.johannes.94@gmail.com>
2019-03-10 14:26:15 +0100

    Map horizontal directions with if/elseif instead of calculation

--- src/mirror/direction.lua
+++ src/mirror/direction.lua
@@ -1,12 +1,22 @@
 Direction = {}
 
 function Direction.mirror_horizontal(direction)
-    if Direction:is_vertical_direction(direction) then
-        return direction
-    elseif Direction:is_horizontal_direction(direction) then
-        return (direction + 4) % 8
-    else
-        return (direction + 2) % 8
+    if direction == defines.direction.north then
+        return defines.direction.north
+    elseif direction == defines.direction.northeast then
+        return defines.direction.northwest
+    elseif direction == defines.direction.east then
+        return defines.direction.west
+    elseif direction == defines.direction.southeast then
+        return defines.direction.southwest
+    elseif direction == defines.direction.south then
+        return defines.direction.south
+    elseif direction == defines.direction.southwest then
+        return defines.direction.southeast
+    elseif direction == defines.direction.west then
+        return defines.direction.east
+    elseif direction == defines.direction.northwest then
+        return defines.direction.northeast
     end
 end
 
