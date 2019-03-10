36966bd7c96b4c25af2434b51ad280dc3a74e81b
Johannes Doll <doll.johannes.94@gmail.com>
2019-03-10 14:25:32 +0100

    Improve logging

--- src/mirror/blueprint_mirror_function.lua
+++ src/mirror/blueprint_mirror_function.lua
@@ -22,6 +22,7 @@ function BluePrintMirrorFunction:mirror_position_horizontal(position)
 end
 
 function BluePrintMirrorFunction:mirror_direction_horizontal(entity)
+    print(self:entity_to_string(entity))
     if entity.direction ~= nil then
         entity.direction = Direction.mirror_horizontal(entity.direction)
     end
@@ -40,9 +41,10 @@ function BluePrintMirrorFunction:mirror_position_vertical(position)
 end
 
 function BluePrintMirrorFunction:mirror_direction_vertical(entity)
-    if entity.direction ~= nil then
-        entity.direction = Direction.mirror_vertical(entity.direction)
-    end
+    print(self:entity_to_string(entity))
+    -- direction may be nil when facing north
+    -- also when rotating a blueprint the directions of its entities do NOT change, the rotation of the blueprint is added instead
+    entity.direction = Direction.mirror_vertical(entity.direction or 0)
 end
 
 function BluePrintMirrorFunction:print_entities()
@@ -57,6 +59,16 @@ function BluePrintMirrorFunction:entity_to_string(entity)
     local position = Point:new(entity.position)
     local name = entity.name
 
-    local result = "Entity: " .. name
-    return result .. " Position: " .. position:to_string()
+    local name = "Entity: " .. name
+    local position = "Position: " .. position:to_string()
+    local direction = "Direction: " .. self:direction_to_string(entity.direction)
+    return name .. " | " .. position .. " | " .. direction
+end
+
+function BluePrintMirrorFunction:direction_to_string(direction)
+    if direction == nil then
+        return "nil"
+    else
+        return game.direction_to_string(direction)
+    end
 end
